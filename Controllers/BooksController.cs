405d764b294c77b794b819caf9f49a3745cbc216
David <David@DESKTOP-EA2B07L>
2022-04-19 14:07:28 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using webapi_csharp.Modelos;
+using webapi_csharp.Repositorios;
+
+namespace webapi_csharp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            RPBooks rpBook = new RPBooks();
+            return Ok(rpBook.ObtenerBooks());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            RPBooks rpBook = new RPBooks();
+
+            var BookRet = rpBook.ObtenerBooks(id);
+
+            if (BookRet == null)
+            {
+                var nf = NotFound("El libro " + id.ToString() + " no existe.");
+                return nf;
+            }
+
+            return Ok(BookRet);
+        }
+
+        [HttpPost]
+        public IActionResult AgregarBook(Book nuevoBook)
+        {
+            RPBooks BooksCli = new RPBooks();
+            
+            var BookRet = BooksCli.ObtenerBooks(nuevoBook.Id);
+
+            if (BookRet == null)
+            {
+                BooksCli.Agregar(nuevoBook);
+                return CreatedAtAction(nameof(AgregarBook), nuevoBook);
+            }
+            else
+            {
+                var nf = BadRequest("El libro " + nuevoBook.Id.ToString() + " ya existe.");
+                return nf;
+            }
+            
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateBook(Book nuevoBook, int Id)
+        {
+            RPBooks rpBook = new RPBooks();
+
+            var BookRet = rpBook.ObtenerBooks(Id);
+
+            if (BookRet == null)
+            {
+                var nf = NotFound("El libro " + Id.ToString() + " no existe.");
+                return nf;
+            }
+
+            rpBook.UpdateBook(nuevoBook, Id);
+
+            return Ok(rpBook.ObtenerBooks(Id));
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBook(int id)
+        {
+            RPBooks rpCli = new RPBooks();
+
+            var cliRet = rpCli.ObtenerBooks(id);
+
+            if (cliRet == null)
+            {
+                var nf = NotFound("El libro " + id.ToString() + " no existe.");
+                return nf;
+            }
+
+            rpCli.DeleteBook(id);
+
+            return Ok(rpCli.ObtenerBooks());
+        }
+    }
+}
---
+++ Repositorios/RPBooks.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using webapi_csharp.Modelos;
+
+namespace webapi_csharp.Repositorios
+{
+    public class RPBooks
+    {
+        public static List<Book> _listaBooks = new List<Book>()
+        {
+            new Book() { Id = 1, Title = "La hojarasca" , Description = "Good one" , Author = "Gabo" },
+            new Book() { Id = 2, Title = "El coronel no tiene quien le escriba" , Description = "Interesting" , Author = "Gabo" },
+        };
+
+        public IEnumerable<Book> ObtenerBooks()
+        {
+            return _listaBooks;
+        }
+
+        public Book ObtenerBooks(int id)
+        {
+            var book = _listaBooks.Where(cli => cli.Id == id);
+
+            return book.FirstOrDefault();
+        }
+
+        public void Agregar(Book nuevoBook)
+        {
+            _listaBooks.Add(nuevoBook);
+        }
+
+        public void UpdateBook(Book uBook, int id)
+        {
+            var book = _listaBooks.Where(cli => cli.Id == id);
+
+            var update = book.FirstOrDefault();
+
+            update.Id = id;
+            update.Author = uBook.Author;
+            update.Description = uBook.Description;
+            update.Title = uBook.Title;
+        }
+
+        public void DeleteBook(int id)
+        {
+            _listaBooks.RemoveAll(cli => cli.Id == id);
+        }
+    }
+}
